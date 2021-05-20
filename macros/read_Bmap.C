088d7bb475b639de18f61840bb1040d7a7e5d22a
Roberto Preghenella <preghenella@bo.infn.it>
2021-05-20 08:36:56 +0200

    Read Bmap also for radial coordinates

--- macros/read_Bmap.C
+++ macros/read_Bmap.C
@@ -30,3 +30,27 @@ read_Bmap()
   gy->SetNpy(500);
   gy->Draw("colz");
 }
+
+void
+read_Bmap_radial_coords()
+{
+
+  auto gr = new TGraph2D("EIC_Magnetic_Field_Map_2021_05_07_radial_coords_[cm]_[T].120000.lines.Bmap", "%lg %lg %lg %*lg");
+  gr->SetTitle("Br (T);z (mm);R (mm)");
+  auto gz = new TGraph2D("EIC_Magnetic_Field_Map_2021_05_07_radial_coords_[cm]_[T].120000.lines.Bmap", "%lg %lg %*lg %lg");
+  gz->SetTitle("Bz (T);z (mm);R (mm)");
+
+  swap(gr);
+  swap(gz);
+
+  auto c = new TCanvas("c", "c", 800, 800);
+  c->Divide(1, 2);
+  c->cd(1);
+  gz->SetNpx(500);
+  gz->SetNpy(500);
+  gz->Draw("colz");
+  c->cd(2);
+  gr->SetNpx(500);
+  gr->SetNpy(500);
+  gr->Draw("colz");
+}
